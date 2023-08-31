d955244cce209a0585f1ce4b331eee7a159339ba
rishgupta2301 <guptarishabh1122@gmail.com>
2023-08-31 19:17:33 +0530

    changes the scss files to styled components

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,8 +1,11 @@
-import { useParams } from "react-router-dom";
-import { useContext, useState, useEffect } from "react";
-import { CategoriesContext } from "../../contexts/categories.context";
-import "./category.style.scss";
-import ProductCard from "../../components/product-card/product-card.component";
+import { useContext, useState, useEffect, Fragment } from 'react';
+import { useParams } from 'react-router-dom';
+
+import ProductCard from '../../components/product-card/product-card.component';
+
+import { CategoriesContext } from '../../contexts/categories.context';
+
+import { CategoryContainer, Title } from './category.style';
 
 const Category = () => {
   const { category } = useParams();
@@ -14,15 +17,15 @@ const Category = () => {
   }, [category, categoriesMap]);
 
   return (
-    <>
-      <h2 className="category-title">{category.toUpperCase()}</h2>
-      <div className="category-container">
+    <Fragment>
+      <Title>{category.toUpperCase()}</Title>
+      <CategoryContainer>
         {products &&
           products.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
-      </div>
-    </>
+      </CategoryContainer>
+    </Fragment>
   );
 };
 
