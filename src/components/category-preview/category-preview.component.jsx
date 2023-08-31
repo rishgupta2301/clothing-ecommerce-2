cf66ded8071107c30480622c9859c713283e03d4
rishgupta2301 <guptarishabh1122@gmail.com>
2023-08-31 14:49:43 +0530

    made category component separate page

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,11 +1,13 @@
 import ProductCard from '../product-card/product-card.component';
-
+import { Link } from 'react-router-dom';
 import './category-preview.style.scss';
 
 const CategoryPreview = ({ title, products }) => (
   <div className='category-preview-container'>
     <h2>
-      <span className='title'>{title.toUpperCase()}</span>
+      <Link className='title' to={title}>
+        {title.toUpperCase()}
+      </Link>
     </h2>
     <div className='preview'>
       {products
