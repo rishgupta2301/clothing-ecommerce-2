324733e7645b6015795cdd727e88ed167aabd981
rishgupta2301 <guptarishabh1122@gmail.com>
2023-08-31 19:55:31 +0530

    added routes from the homepage

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,17 +1,24 @@
+import { useNavigate } from "react-router-dom";
 import {
   BackgroundImage,
   Body,
   DirectoryItemContainer,
-} from './directory-item.styles';
+} from "./directory-item.styles";
 
 const DirectoryItem = ({ category }) => {
-  const { imageUrl, title } = category;
+  const { imageUrl, title, route } = category;
+  const navigate = useNavigate();
+
+  const onNavigateHandler = () => navigate(route);
+
   return (
-    <DirectoryItemContainer>
+    <DirectoryItemContainer onClick={onNavigateHandler}>
       <BackgroundImage imageUrl={imageUrl} />
       <Body>
-        <h2>{title}</h2>
-        <p>Shop Now</p>
+        {/* <Link to={`shop/${title}`}> */}
+          <h2>{title}</h2>
+          <p>Shop Now</p>
+        {/* </Link> */}
       </Body>
     </DirectoryItemContainer>
   );
